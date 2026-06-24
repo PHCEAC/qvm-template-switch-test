2cac46584d79c73f60b8cc234fefd6c6486ec2a0
CharlieC <85197472+PHCEAC@users.noreply.github.com>
2026-06-25 01:09:13 +0200

    Update qvm-template-switch-test.sh WIP
    Do not use..

--- qvm-template-switch-test.sh
+++ qvm-template-switch-test.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-qvm-shutdown sys-usb
+# get the args sorted:
+for i in "$@" ; do
+case $i in
+    -p=*|--prefix=*)
+    todo
+# todo : get the reversion tpl
+# guess : what is target vn
+# todo 
+qvm-shutdown --wait sys-usb
 qvm-prefs sys-usb template fedora-43-xfce
 qvm-start sys-usb
-sleep 3m
-qvm-shutdown sys-usb
+sleep 3m # cannot assume human readable times?
+qvm-shutdown --wait sys-usb
 qvm-prefs sys-usb template fedora-42-xfce
 qvm-start sys-usb
